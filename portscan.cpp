9e6dce35247ca252b6419e5298267ecc7a05607e
marisa <mlsig95@gmail.com>
2020-09-17 16:35:23 -0400

    comment skeleton for code

---
+++ portscan.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    int running = 0
+    //while running true
+      //prompt ip input
+      //if they are done, running false
+      //else
+        //check valid ip (need regex, dw if you don't know it)
+        //if valid
+          //run scan function on ip
+        //else, messgae + continue
+    //closing messgae
+    return 0;
+}
+
+//concurrently scanning function here - input: IP
