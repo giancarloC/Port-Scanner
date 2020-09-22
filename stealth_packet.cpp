9c7b309c5099259340bf4e3a7760510af29bba3f
Andres Orbe <aeo34@njit.edu>
2020-09-21 22:06:32 -0400

    file factored as cpp

--- stealth_packet.cpp
+++ stealth_packet.cpp
@@ -4,21 +4,23 @@
 using namespace Tins;
 using namespace std;
 
-// The sender
-PacketSender sender;
-// The SYN to be sent.
-IP pkt = IP("192.168.0.1") / TCP(22, 1337);
-pkt.rfind_pdu<TCP>().set_flag(TCP::SYN, 1);
+int main(int argc, char* argv[]) {
+    // The sender
+    PacketSender sender;
+    // The FIN to be sent.
+    IP pkt = IP("192.168.0.1") / TCP(22, 1337);
+    pkt.rfind_pdu<TCP>().set_flag(TCP::FIN, 1);
 
-// Send and receive the response.
-std::unique_ptr<PDU> response(sender.send_recv(pkt));
-// Did we receive anything?
-if (response) {
-    TCP &tcp = response->rfind_pdu<TCP>();
-    if (tcp.get_flag(TCP::RST)) { 
-        std::cout << "Port is closed!" << std::endl;
-    }
-    else {
-        std::cout << "Port is open!" << std::endl;
+    // Send and receive the response.
+    std::unique_ptr<PDU> response(sender.send_recv(pkt));
+    // Did we receive anything?
+    if (response) {
+        TCP &tcp = response->rfind_pdu<TCP>();
+        if (tcp.get_flag(TCP::RST)) { 
+            std::cout << "Port is closed!" << std::endl;
+        }
+        else {
+            std::cout << "Port is open!" << std::endl;
+        }
     }
 }
